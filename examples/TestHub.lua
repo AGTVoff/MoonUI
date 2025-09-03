abe37707c45f8f1cad64a899007ce13e7038b18b
AGTVoff <aaronnallet2104@gmail.com>
2025-09-03 19:51:34 +0200

    Update TestHub.lua

--- examples/TestHub.lua
+++ examples/TestHub.lua
@@ -1,17 +1,17 @@
--- rlly easy example for MoonUI
+-- rlly easy example for MoonUI Dark / Clean
 
 local MoonUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/AGTVoff/MoonUI/refs/heads/main/MoonUI.lua"))()
 local Window = MoonUI:CreateWindow("Test Hub")
 
--- this is Tabs
+-- Tabs
 local TabAIM = Window:CreateTab("AIM")
 local TabVisual = Window:CreateTab("VISUAL")
 
--- variables for toggle
+-- Variables
 local AimbotToggle = false
 local WallhackToggle = false
 
--- toggles creation
+-- Toggles
 Window:CreateToggle(TabAIM, "Aimbot", false, function(state)
     AimbotToggle = state
 end)
@@ -20,18 +20,23 @@ Window:CreateToggle(TabVisual, "Wallhack", false, function(state)
     WallhackToggle = state
 end)
 
--- simple button I did a speedhack for example
+-- Slider exemple (optionnel)
+Window:CreateSlider(TabAIM, "Smoothness", 1, 20, 10, function(value)
+    print("Smoothness set to", value)
+end)
+
+-- button (I did a speedhack for example)
 Window:CreateButton(TabAIM, "Set Speed to 50", function()
-    local LocalPlayer = game.Players.LocalPlayer
-    if LocalPlayer.Character and LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
-        LocalPlayer.Character:FindFirstChildOfClass("Humanoid").WalkSpeed = 50
+    local player = game.Players.LocalPlayer
+    if player.Character and player.Character:FindFirstChild("Humanoid") then
+        player.Character.Humanoid.WalkSpeed = 50
     end
 end)
 
--- RenderStep pour aimbot et wallhack
+-- RenderStepped pour Aimbot et Wallhack
 game:GetService("RunService").RenderStepped:Connect(function()
+    local LocalPlayer = game.Players.LocalPlayer
     if AimbotToggle then
-        local LocalPlayer = game.Players.LocalPlayer
         local closest
         local minDist = math.huge
         for i,v in pairs(game.Players:GetPlayers()) do
@@ -50,7 +55,7 @@ game:GetService("RunService").RenderStepped:Connect(function()
 
     if WallhackToggle then
         for i,v in pairs(game.Players:GetPlayers()) do
-            if v ~= game.Players.LocalPlayer and v.Character and v.Character:FindFirstChild("HumanoidRootPart") then
+            if v ~= LocalPlayer and v.Character and v.Character:FindFirstChild("HumanoidRootPart") then
                 local part = v.Character.HumanoidRootPart
                 if not part:FindFirstChild("Highlight") then
                     local hl = Instance.new("Highlight")
